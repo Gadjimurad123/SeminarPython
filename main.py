9e95950d8f950fac49831575ca8a5d40bc4d682f
Gadjimurad Aliev <BeanOfficial@list.ru>
2023-06-20 21:03:16 +0300

    Создал файл main

---
+++ main.py
@@ -0,0 +1,13 @@
+# n = int(input("Введите число: "))
+# print(f'{n} * 5 = {n * 5}')
+
+# n = int(input("Введите первое число: "))
+# m = int(input("Введите второе число: "))
+# if n > m:
+#     print(n)
+# elif n < m:
+#     print(m)
+# else:
+#     print(m)
+
+
